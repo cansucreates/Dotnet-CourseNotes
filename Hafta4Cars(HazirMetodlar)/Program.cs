f80474266de1864658d3c6e77c4dec5eb6af77f1
caysagdic@gmail.com <caysagdic@gmail.com>
2024-03-23 11:57:18 +0300

    #

--- Hafta4Cars(HazirMetodlar)/Program.cs
+++ Hafta4Cars(HazirMetodlar)/Program.cs
@@ -44,6 +44,8 @@ namespace Hafta4Cars_HazirMetodlar_
             metin1.CopyTo(0, metin4, 0, 5); // metin1'in 0. karakterinden itibaren 5 karakterini metin4'e kopyalar.
 
 
+
+
             
             
              
